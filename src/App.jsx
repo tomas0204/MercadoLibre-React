b8db84fbdfe47a92092e0c18f3c3c84a3f5c4ef5
Tomás <149749382+tomas0204@users.noreply.github.com>
2025-10-26 22:06:08 -0300

    Home -> App (Products and Carousel)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import FooterML from './components/shared/Footer'
+import Menu from './components/shared/Menu'
+import Home from './components/pages/Home' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
   return (
     <>
-      
+      <Menu />
+      <Home />
       <FooterML />
     </>
   )
